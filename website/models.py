36d120541924ee0752e9c4a90935fdb6f899a1b4
Jonathan Cunningham <jonathan.cunning@ufl.edu>
2022-08-04 10:00:31 -0400

    Comment Posting
    - Updated database to include Comments
    - Added a Comment database model
    - Updated post_div to display comments, and a post comment button
    - Added a create-comment view for comment creation

--- website/models.py
+++ website/models.py
@@ -10,9 +10,18 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     posts = db.relationship('Post', backref='user', passive_deletes=True) # Stores posts made by this user, which can be backreferenced through post.user
+    comments = db.relationship('Comment', backref='user', passive_deletes=True)
 
 class Post(db.Model): # Post model
     id = db.Column(db.Integer, primary_key=True) # ID of the post
     text = db.Column(db.Text, nullable=False) # Text of the post, cannot be null
     date_created = db.Column(db.DateTime(timezone=True), default=func.now()) # Date post was made
     author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="Cascade"), nullable=False) # User who made post, is foreign key
+    comments = db.relationship('Comment', backref='post', passive_deletes=True)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # ID of the Comment
+    text = db.Column(db.String(200), nullable=False) # Text of the Comment, cannot be null, 200 chars
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now()) # Date comment was made
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="Cascade"), nullable=False) # User who made comment
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id', ondelete="Cascade"), nullable=False) # Post comment is on
