24da43291f4434f938c5c482560f6af5184c52a1
Jonathan Cunningham <jonathan.cunning@ufl.edu>
2022-08-03 20:04:22 -0400

    Post Creation
    - Added Post database model
    - Added a create-post page, where users can submit a post
    - Added create-post page to views with post logic

--- website/models.py
+++ website/models.py
@@ -9,3 +9,10 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    posts = db.relationship('Post', backref='user', passive_deletes=True) # Stores posts made by this user, which can be backreferenced through post.user
+
+class Post(db.Model): # Post model
+    id = db.Column(db.Integer, primary_key=True) # ID of the post
+    text = db.Column(db.Text, nullable=False) # Text of the post, cannot be null
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now()) # Date post was made
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="Cascade"), nullable=False) # User who made post, is foreign key
--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
 # webpages / blueprint
 # store views route
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Post
+from . import db
 
 views = Blueprint("views", __name__)
 
@@ -12,3 +14,17 @@ views = Blueprint("views", __name__)
 def home():
     return render_template("home.html", user=current_user)
     # flask knows to search for templates in folders called "templates"
+
+@views.route("/create-post", methods=['GET', 'POST']) # Route for creating post
+@login_required # Must be logged in
+def create_post():
+    if request.method== "POST": # If Post button is clicked
+        text = request.form.get('text') # Get text from form
+        if not text: # Text cannot be empty
+            flash('Post cannot be empty', category='error')
+        else: 
+            post = Post(text=text, author=current_user.id) # Create post
+            db.session.add(post) # Add post to database
+            db.session.commit()
+            flash('Post created!', category='success')
+    return render_template('createpost.html', user=current_user) # Go to create post page, pass user
